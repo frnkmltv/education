25ecc996a8564d6ab98066ef5cba50a2ed937a89
frankmolotov <molkonigo@gmail.com>
2017-06-23 08:04:29 +0300

    monitor done

--- opos/gui.py
+++ opos/gui.py
@@ -1,87 +1,100 @@
 import Tkinter as tk
-import sys
 import threading
-import time
-import tkMessageBox
 
 import operations
 
 
-class GUI():
+class GUI(tk.Frame):
     master = tk.Tk()
-    running = True
+    keys = dict.fromkeys(
+        ["PID", "Mem Usage", "Status", "CPU Time",
+         "Window Title"], None
+    )
 
     def on_closing(self):
-        if tkMessageBox.askokcancel("Quit", "Do you want to quit?"):
-            sys.exit(0)
+        import tkMessageBox, sys
+        try:
+            process_item_values = operations.Process(filename=self.filename).current_state
+            if tkMessageBox.askokcancel("Quit",
+                                        "Do you want to quit?\nMonitored process is %s state" % process_item_values[
+                                            'Status'].split('\n')[0]):
+                sys.exit(0)
+        except AttributeError:
+            if tkMessageBox.askokcancel("Quit",
+                                        "Do you want to quit?"):
+                sys.exit(0)
 
     def stop_monitor(self):
-        self.runnning = False
+        self.running = False
+        self.stop_button.configure(command=self.on_closing, text='Exit')
+        self.stop_button.grid_configure(row=2, column=1)
+        self.second_thread.join(timeout=1)
 
-    def __init__(self, master=None, size='800x600'):
-        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.width = size.split('x')[0]
-        self.height = size.split('x')[1]
+    def __init__(self, master=master, size='450x300', running=False):
+        self.running = running
+        tk.Frame.__init__(self, master)
         self.master.geometry(size)
+        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.width = self.master.winfo_width()
+        self.height = self.master.winfo_height()
         self.master.title('Process monitor')
-        self.info = tk.StringVar()
-        self.info.set('Check if process active...')
         self.program_name = tk.StringVar()
         self.program_name.set('Enter File Name [.exe] or Window Caption: ')
-        self.ask_label = tk.Label(self.master, textvariable=self.program_name,
-                                  height=int(int(self.height) / 200),
-                                  font=('Times New Roman', '10'), width=int(int(self.width) / 15))
-
-        self.ask_entry = tk.Entry(self.master, width=int(int(self.width) / 15))
+        self.ask_frame = tk.Frame(self.master,
+                                  highlightbackground='black', relief='ridge', bd='5', bg='white', width=self.width)
+        self.display_process_frame = tk.Frame(self.master,
+                                              highlightbackground='black', bg='white', relief='ridge', bd='2',
+                                              width=self.width)
+        self.ask_label = tk.Label(self.ask_frame, textvariable=self.program_name,
+                                  font=('Ubuntu', '12', 'bold'), bg='white')
+        self.ask_entry = tk.Entry(self.ask_frame, width=61, font=('Ubuntu', '10', 'bold'), bd='5')
         self.main = threading.Thread(target=self.master.mainloop, name='main')
-        self.window_thread = threading.Thread(target=self.change_window, name='windowthread')
-        self.window_thread.daemon = True
-        try:
-            print(self.filename, 'est')
-        except:
-            print('netu')
-        try:
-            print(self.window_thread.is_alive())
-        except:
-            print('netu')
-
-        self.ask_button = tk.Button(self.master, text='Monitor',
-                                    command=self.window_thread.run)
+        self.ask_button = tk.Button(self.ask_frame, text='Monitor', width=10, pady=2,
+                                    command=self.change_window, relief=tk.GROOVE, font=('Ubuntu', '8', 'bold'))
+        self.stop_button = tk.Button(self.ask_frame, text='Stop', width=10, pady=2, relief=tk.GROOVE,
+                                     font=('Ubuntu', '8', 'bold'))
         self.main.start()
-        print(threading.current_thread(), 'current')
-        if self.window_thread.is_alive():
-            self.window_thread.join()
-        self.ask_label.grid(row=0, column=0)
-        self.ask_entry.grid(row=1, column=0)
+        self.ask_frame.grid(row=0, column=0)
+        self.ask_label.grid(row=0, column=0, columnspan=2)
+        self.ask_entry.grid(row=1, column=0, columnspan=2)
         self.ask_button.grid(row=2, column=0)
 
     def change_window(self):
-        self.filename = self.ask_entry.get()
-        print(threading.enumerate())
-        print(self.filename, 'here is filename')
-        if self.filename != '' and self.filename != 'None':
+        self.running = True
+        filename = self.ask_entry.get()
+        if filename != '' and filename != 'None':
+            self.filename = filename
             self.second_thread = threading.Thread(target=self.start_monitor, name='monitorthread')
             self.second_thread.daemon = True
             self.second_thread.start()
+            self.stop_button.configure(command=self.stop_monitor, text='Stop')
+            self.stop_button.grid_configure(row=2, column=1)
         else:
             self.program_name.set('Enter File Name [.exe] or Window Caption: ' + '\nEmpty field. Try again')
-            if self.filename == '':
-                self.main.run()
-                self.ask_button.configure(command=self.window_thread.run)
-                self.ask_button.grid_configure(row=2, column=0)
+
+            self.ask_button.grid_configure(row=2, column=0)
 
     def start_monitor(self):
-        self.program_name.set('Status of "%s" process: ' % self.filename)
-        self.list_label = tk.Label(self.master, textvariable=self.info, bg='white', width=int(int(self.width) / 15),
-                                   fg='black', height=int(int(self.height) / 10))
-        self.list_label.grid(row=0, column=1, rowspan=4)
-        # threading.Thread(target=process, args={'filename': filename}).start()
-        while True:
-            task = self.info.get()
-            time.sleep(1)
-            proc = operations.Process(filename=self.filename)
-            if proc.current_state != task:
-                self.info.set(task + '\n' + proc.current_state)
+        self.display_process_frame.grid(row=1, column=0)
+        self.program_name.set('Status of %s process: ' % self.filename.upper())
+        co = 0
+        for label in self.keys.keys():
+            if not self.keys[label]:
+                self.keys[label] = tk.StringVar()
+                self.list_label = tk.Label(self.display_process_frame, text=label, width=len(str(label)) + 3, anchor=tk.W,
+                                           fg='black',
+                                           font=('Times New Roman', '10', 'bold'))
+            self.values_label = tk.Message(self.display_process_frame, textvariable=self.keys[label], bg='white',
+                                           fg='black',
+                                           font=('Times New Roman', '10'))
+            self.list_label.grid(row=0, column=co)
+            self.values_label.grid(row=1, column=co)
+            # threading.Thread(target=process, args={'filename': filename}).start()
+            co += 1
+        while self.running:
+            process_item_values = operations.Process(filename=self.filename).current_state
+            for key in self.keys.keys():
+                self.keys[key].set(process_item_values[key])
 
 
 if __name__ == '__main__':
--- opos/operations.py
+++ opos/operations.py
@@ -1,36 +1,58 @@
-import time
-
 import os
 
 
 class Process:
-    run = True
-
-    def __init__(self, filename, ask_delay=5):
-        self.ask_delay = ask_delay
+    def __init__(self, filename):
         self.filename = filename
-        self.current_state = 'Unknown'
-        info = os.popen('tasklist /FI "IMAGENAME eq %s" /fo  csv /v /nh' % self.filename).read()
-        if not info.startswith('INFO: No tasks are running which match the specified criteria.'):
-            subtasks = [i for i in info.split('\n')]
-            if len(subtasks) > 1:
-                self.window_caption = subtasks[0].split(',')[-1]
-                self.subtasks = subtasks[1:]
-            self.state = info.split(',')[6]
-            print(len(info.split('\n')) - 1)
-            print(info)
-            print(self.__dict__)
-            self.current_state = info
+        self.current_state = dict.fromkeys(
+            ["PID", "Mem Usage", "Status", "CPU Time",
+             "Window Title"], ''
+        )
+        if self.filename.endswith('.exe'):
+            info = os.popen('tasklist /FI "IMAGENAME eq %s" /fo  csv /v  /nh' % self.filename).read()
         else:
-            self.current_state = 'Not running yet.'
-        time.sleep(self.ask_delay)
+            info = os.popen('tasklist /FI "WINDOWTITLE eq %s" /fo  csv /v  /nh' % self.filename).read()
+        subtasks = [i for i in info.split('\n')]
+        if not info.startswith('INFO: No tasks are running which match the specified criteria'):
+            if len(subtasks) > 1:
+                self.current_state["Window Title"] += subtasks[0].split(',')[-1][1:-1] + '\n'
+                for i in range(len(subtasks) - 1):
+                    self.current_state["CPU Time"] += subtasks[i].split(',')[-2][1:-1] + '\n'
+                    self.current_state["Status"] += subtasks[i].split(',')[6][1:-1] + '\n'
+                    self.current_state["Mem Usage"] += subtasks[i].split(',')[5][1:-1] + '\n'
+                    self.current_state["PID"] += subtasks[i].split(',')[1][1:-1] + '\n'
+            else:
+                self.current_state["Window Title"] = subtasks[0].split(',')[0][1:-1]
+                self.current_state["CPU Time"] = subtasks[0].split(',')[8][1:-1]
+                self.current_state["Status"] = subtasks[0].split(',')[6][1:-1]
+                self.current_state["Mem Usage"] = subtasks[0].split(',')[5][1:-1]
+                self.current_state["PID"] = subtasks[0].split(',')[1][1:-1]
+                # print(len(info.split('\n')) - 1)
+                # print(info)
+                # print(self.__dict__)
 
-        # def check(self):
-        #     while self.run:
-        #         # info = os.popen(
-        #         #     'wmic process where "name="%s"" get ExecutablePath' % self.filename).read()
-        #         # print(os.popen(self.filename).read())
-        #
+        else:
+            self.current_state["Status"] = 'Not running'
+            self.current_state["Window Title"] = '-----'
+            self.current_state["PID"] = '-----'
+            self.current_state["CPU Time"] = '-----'
+            self.current_state["Mem Usage"] = '-----'
+
+
+            # else:
+            #     if self.current_state['PID'] == None:
+            #         pass
+            #     elif not self.current_state:
+            #         self.current_state = 'Not running yet.'
+            #     else:
+            #         self.current_state = 'Task has stopped.'
+
+            # def check(self):
+            #     while self.run:
+            #         # info = os.popen(
+            #         #     'wmic process where "name="%s"" get ExecutablePath' % self.filename).read()
+            #         # print(os.popen(self.filename).read())
+            #
 
 
 if __name__ == '__main__':
